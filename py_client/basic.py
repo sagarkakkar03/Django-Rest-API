bde454ca1f91f9335d9ba951bdbc2515825763d8
sagarkakkar03 <122864251+sagarkakkar03@users.noreply.github.com>
2023-09-22 14:27:11 +0530

    Update basic.py

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,6 +1,8 @@
 import requests
 
-endpoint = "https://httpbin.org/anything"
-
-get_response = requests.get(endpoint)
-print(get_response.text)
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api"
+get_response = requests.get(endpoint, params={'abc':123}, json={"query": "Hello world"})
+# print(get_response.text)
+# print(get_response.status_code)
+print(get_response.json())
